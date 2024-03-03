661d83d35e12ec613737dae6fd2029b75c060b77
Dmaur <Derrickmaurais1@gmail.com>
2024-03-02 20:34:22 -0400

    got the drop menu working and populated

--- src/main.js
+++ src/main.js
@@ -1,5 +1,44 @@
 // importing the sass stylesheet for bundling
 import "./../sass/styles.scss";
+import { getJSONData } from "./Toolkit";
 
-// JS content goes here
-// ...
+const CITY_LIST ="http://localhost:3000/cities.json";
+let dropDownMenu;
+
+function popDrop(data){
+    let citiesJson = JSON.stringify(data);
+    let cityListItem = JSON.parse(citiesJson);
+    // console.log(cityListItem.data.cities[3].province);
+    cityListItem.data.cities.forEach(e => {
+        console.log(`${e.name}, ${e.province}`);
+        let dropCity = document.createElement("option");
+        dropCity.className = "g-drop-city";
+        dropCity.textContent = `${e.name}, ${e.province}`;
+        dropDownMenu.appendChild(dropCity);
+    });
+ 
+    // console.log(cityListItem.data.cities.length);
+    //     let dropCity = document.createElement("div");
+    //     dropCity.className = "s-drop_city";
+    //     dropCity.innerHTML = `${cityListItem.data.cities[i].name}, ${cityListItem.data.cities[i].province}`;
+    //     document.dropDownMenu.appendChild(dropCity);
+    // }
+
+
+}
+
+
+
+
+function onError(e){
+    console.log("*** Error has occurred during AJAX data retrival");
+}
+
+function main(){
+    dropDownMenu = document.getElementById("s-drop-down-menu");
+
+    document.addEventListener("DOMContentLoaded", getJSONData(CITY_LIST,popDrop, onError));
+
+//     getJSONData(CITY_LIST,popDrop, onError);
+}
+main();
