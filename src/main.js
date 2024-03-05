02d5f08de8da28ee19c99d4df83f94d1a16dd388
Dmaur <Derrickmaurais1@gmail.com>
2024-03-05 09:39:43 -0400

    everything but icons is done

--- src/main.js
+++ src/main.js
@@ -1,13 +1,21 @@
 // importing the sass stylesheet for bundling
 import "./../sass/styles.scss";
 import { getJSONData } from "./Toolkit";
+// import "./weather-icons-wind.min.css";
+// import "./weather-icons.min.css";
+
+import { Spinner } from "spin.js";
+import "spin.js/spin.css";
 
 const CITY_LIST ="http://localhost:3000/cities.json";
+// variables to do the dropdown menu work. 
 let dropDownMenu, citiesJson;
 
 // declaring all the variables that need to be filled with weather info
-let sTemp,icon,sky, current, low, high, feelsLike, vis, km, humidity,percent,airP,hPa,wind,degree,windSpeed;
-
+let sTemp,icon,sky, current, low, high, feelsLike, vis, km, humidity,percent,airP,hPa,wind,degree,windSpeed, location;
+let city;
+let loadingOverlay;
+let spinner = new Spinner({ color: "#FFFFFF", lines: 12 }).spin(document.querySelector(".g-loading-overlay"));
 
 
 
@@ -25,10 +33,69 @@ function popDrop(data){
         dropCity.textContent = `${e.name}, ${e.province}`;
         dropDownMenu.appendChild(dropCity);
     });
+    // turn off initial loading overlay
+    loadingOverlay.style.display = "none";
+}
+    
+function grabUserSelect(){
+    //  grabs the value of the city selected by user
+    let optionSelected = dropDownMenu.options[dropDownMenu.selectedIndex];
+    let optionHtml = optionSelected.innerHTML;
+    // stripping the province name from the selected info then using it to get the weather info from API
+    let locationArray = optionHtml.split(",");
+    let cityName = locationArray[0].trim();
+    location.innerHTML = optionSelected.innerHTML;
+    // save user selection to local storage
+    window.localStorage.setItem("location",locationArray);
+    
+    return cityName;
+
 }
 
-function getWeather(){
+function displayWeather(city){
     
+    const WEATHER_JSON =`https://api.openweathermap.org/data/2.5/weather?q=${city},CA&appid=c62569daba1249f5420a17b2f0030224&units=metric`;
+
+    getJSONData(WEATHER_JSON, e=>{  
+        let weatherJson = JSON.stringify(e);
+        let weatherparsed = JSON.parse(weatherJson);
+        //----check to see if city data exists
+        if (weatherparsed.cod == "404"){
+            alert(`${weatherparsed.message}`);
+            document.getElementById("sky").innerHTML= `${weatherparsed.message}`;
+            document.getElementById("location").innerHTML = `${weatherparsed.message}`;
+            document.getElementById("s-weather-details").style.display = "none";
+        }else{
+            document.getElementById("s-weather-details").style.display = "flex";
+            current.innerHTML = `Current&nbsp; ${weatherparsed.main.temp}&deg;C`;
+            low.innerHTML = `Low&nbsp; ${weatherparsed.main.temp_min}&deg;C `;
+            high.innerHTML = `high&nbsp; ${weatherparsed.main.temp_max}&deg;C `;
+            feelsLike.innerHTML = `Feels Like&nbsp; ${weatherparsed.main.feels_like}&deg;C &nbsp;`;
+            sky.innerHTML = `${weatherparsed.weather[0].description.toUpperCase()}`;
+            km.innerHTML = `${weatherparsed.visibility / 1000} &nbsp; KM`;
+            percent.innerHTML = `${weatherparsed.main.humidity} &nbsp; %`;
+            hPa.innerHTML = `${weatherparsed.main.pressure} &nbsp; hPa`;
+            degree.innerHTML =`${weatherparsed.wind.deg}&deg;`;
+            windSpeed.innerHTML =`${weatherparsed.wind.speed} km/hr`;
+            document.querySelector("body").style.color = "black";
+            // location.innerHTML =`${weatherparsed.name.toUpperCase()}`;
+            
+
+
+        }
+
+        
+    },onError);
+    
+}
+
+
+
+function onError(e){
+    console.log("*** Error has occurred during AJAX data retrival");
+}
+
+function main(){
     
     // hooking up all the variables to be filled with JSON data to the HTML DOM elements 
     sTemp = document.getElementById("s-temp");
@@ -47,83 +114,51 @@ function getWeather(){
     wind = document.getElementById("wind");
     degree = document.getElementById("degree");
     windSpeed= document.getElementById("windSpeed");
+    location = document.getElementById("location");
 
-    
-    
-    //  grabs the value of the selected city
-    let optionSelected = dropDownMenu.options[dropDownMenu.selectedIndex];
-    let optionHtml = optionSelected.innerHTML;
-
-    // stripping the province name from the selected info then using it to get the weather info from API
-    let locationArray = optionHtml.split(",");
-    let cityName = locationArray[0].trim();
-    const WEATHER_JSON =`https://api.openweathermap.org/data/2.5/weather?q=${cityName},CA&appid=c62569daba1249f5420a17b2f0030224&units=metric`;
-    getJSONData(WEATHER_JSON, e=>{  
-        console.log(e);
-        let weatherJson = JSON.stringify(e);
-        let weatherparsed = JSON.parse(weatherJson);
-        //----check to see if city exists
-        if (weatherparsed.cod == "404"){
-            // alert(`${weatherparsed.message}`);
-            document.getElementById("location").innerHTML = `${weatherparsed.message}`;
-        }else{
-            current.innerHTML = `Current&nbsp; ${weatherparsed.main.temp}&deg;C`;
-            low.innerHTML = `Low&nbsp; ${weatherparsed.main.temp_min}&deg;C `;
-            high.innerHTML = `high&nbsp; ${weatherparsed.main.temp_max}&deg;C `;
-            feelsLike.innerHTML = `Feels Like&nbsp; ${weatherparsed.main.feels_like}&deg;C &nbsp;`;
-            sky.innerHTML = `${weatherparsed.weather[0].description.toUpperCase()}`;
-            km.innerHTML = `${weatherparsed.visibility} &nbsp; KM`;
-            percent.innerHTML = `${weatherparsed.main.humidity} &nbsp; %`;
-            hPa.innerHTML = `${weatherparsed.main.pressure} &nbsp; hPa`;
-            degree.innerHTML = `${weatherparsed.wind.deg}&deg;`;
-            windSpeed.innerHTML = `${weatherparsed.wind.speed}&nbsp; km/h`;
-
-
-
-
-
-            
-            console.log("the city info is there");
-        }
-        console.log(weatherparsed);
-
-        
-    },onError);
-    
-    //setting the inner HTML of the div displaying the selected city
-    document.getElementById("location").innerHTML= optionHtml;
-    
-
-    
-    console.log(optionHtml);
-}
-
-
-
-function onError(e){
-    console.log("*** Error has occurred during AJAX data retrival");
-}
-
-function main(){
-    
+    loadingOverlay = document.querySelector(".g-loading-overlay");
     dropDownMenu = document.getElementById("s-drop-down-menu");
 
- 
+    document.addEventListener("DOMContentLoaded", function(){
+        // check local storage to see if there is a city saved
+        if (localStorage.getItem("location")== null){
+            city = "notfound";
+            displayWeather(city);
+        } else {
+            // if there is a city in local storage display the name and province and pass the city to display weather
+            let SavedCity = localStorage.getItem("location");
+            let justTheCity = SavedCity.split(",");
+            city = justTheCity[0].trim("");
+            location.innerHTML = SavedCity;
+            displayWeather(city);
+        }
+        getJSONData(CITY_LIST,popDrop, onError);
+    }
+    );
 
-
-    document.addEventListener("DOMContentLoaded", getJSONData(CITY_LIST,popDrop, onError));
+    // event listener for when user selects a city 
     dropDownMenu.addEventListener("change", e=>{
         if (e.target.id === "s-drop-down-menu"){
-            getWeather();
+            // grey out text when drop down option changed
+            document.querySelector("body").style.color = "grey";
+            city = grabUserSelect();
+            displayWeather(city);       
         }
+        
     });
-    // let scName = selectedCity.querySelector("div:nth-child(3)");
-    // console.log(scName);
-
-
-    
-    
-
-//     getJSONData(CITY_LIST,popDrop, onError);
 }
 main();
+
+
+
+// current.innerHTML = " ";
+            // low.innerHTML = " ";
+            // high.innerHTML = " ";
+            // feelsLike.innerHTML = " ";
+            // sTemp.textContent = " ";
+            // sky.innerHTML = " ";
+            // km.innerHTML = " ";
+            // humidity.innerHTML = " ";
+            // hPa.innerHTML = " ";
+            // degree.innerHTML = " ";
+            // windSpeed.innerHTML = " ";
